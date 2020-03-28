51a30d00fe2b917a27f8f7d966f2e609b53dc187
Daniel Bowden <github@bowden.in>
2020-03-28 11:41:55 +1100

    Initial project setup

---
+++ DataFeed/ViewControllers/DataFeedViewController.swift
@@ -0,0 +1,14 @@
+//
+//  DataFeedViewController.swift
+//  DataFeed
+//
+//  Created by Daniel Bowden on 28/3/20.
+//  Copyright © 2020 Daniel Bowden. All rights reserved.
+//
+
+import UIKit
+
+class DataFeedViewController: UIViewController {
+    
+    
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+# DataFeed Demo Project
+
+# Author
+**Daniel Bowden**
+
+github.com/danielbowden
+
+twitter.com/danielgbowden
+
+danielbowden.com.au
