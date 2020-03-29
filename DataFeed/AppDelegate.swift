ed59e96acdb6770be9bc53235cb602d8901daea6
Daniel Bowden <github@bowden.in>
2020-03-29 19:59:56 +1100

    App instructions, description and a screenshot

---
+++ DataFeed/AppDelegate.swift
@@ -0,0 +1,37 @@
+//
+//  AppDelegate.swift
+//  DataFeed
+//
+//  Created by Daniel Bowden on 28/3/20.
+//  Copyright © 2020 Daniel Bowden. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+        let rootViewController = DataFeedViewController()
+        let navigationController = UINavigationController(rootViewController: rootViewController)
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
+
+        return true
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) { }
+
+    func applicationDidEnterBackground(_ application: UIApplication) { }
+
+    func applicationWillEnterForeground(_ application: UIApplication) { }
+
+    func applicationDidBecomeActive(_ application: UIApplication) { }
+
+    func applicationWillTerminate(_ application: UIApplication) { }
+}
+
--- README.md
+++ README.md
@@ -1,10 +1,40 @@
 # DataFeed Demo Project
 
-# Author
+![AppIcon](DataFeed/Resources/Assets.xcassets/AppIcon.appiconset/Icon-App-29x29@2x.png)
+A simple app that fetches JSON and displays the results and images in a tableview.
+
+## Instructions
+Open `DataFeed.xcodeproj` in Xcode 11
+
+To run the application, `Build & Run` on the desired iPhone and iPad simulator (cmd+R)
+
+To execute all unit tests, `Build & Test` on the desired device (cmd+U)
+
+## Functionality
+* MVVM Architecture
+* Supports iOS 11.0+
+* Supports Xcode 11
+* Supports iPhone and iPad
+* Supports portrait and landscape orientation
+* Supports dark mode (iOS 13.0+ only)
+* Pull to refresh
+* Unit testing
+* No third-party dependencies
+* Decodable JSON parsing
+* xcconfig configuration
+* URLSession based networking
+* Dynamic height tableview cells
+* Programmatic auto layout, no storyboards or xibs
+* Async image downloading
+* Error handling
+
+## Author
 **Daniel Bowden**
 
-github.com/danielbowden
+www.github.com/danielbowden
 
-twitter.com/danielgbowden
+www.twitter.com/danielgbowden
 
-danielbowden.com.au
+www.danielbowden.com.au
+
+![AppIcon](Screenshot.png)
